ec2b151e3a7b66afcc6e191b38b7101b4485c77d
Carlos Renan <carlosrenan96@gmail.com>
2023-07-24 23:53:32 -0300

    feat: adding albums section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import { Home as HomeIcon, Search, Library} from 'lucide-react';
+import {
+  Home as HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,39 +14,170 @@ export default function Home() {
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950  p-6">
           <div className="flex items-center gap-2">
-            <div className='w-3 h-3 bg-red-500 rounded-full'/>
-            <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
-            <div className='w-3 h-3 bg-green-500 rounded-full'/>
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
           </div>
-          <nav className='space-y-5 mt-10'>
-            <a href="#" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+          <nav className="space-y-5 mt-10">
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <HomeIcon />
               Home
             </a>
-            <a href="#" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <Search />
               Search
             </a>
-            <a href="#" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <Library />
               Your Library
             </a>
           </nav>
 
-          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>Sons Bons</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>Donkey Kong Fav</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>My Playlist #14</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>Antigas</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>YT Music Free</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>POP Antiga</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>FellSongGood</a>
-            <a href="#" className='text-sm text-zinc-400 hover:text-zinc-100'>Só Rock</a>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Sons Bons
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Donkey Kong Fav
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              My Playlist #14
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Antigas
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              YT Music Free
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              POP Antiga
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              FellSongGood
+            </a>
+            <a href="#" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Só Rock
+            </a>
           </nav>
         </aside>
-        <main className="flex-1  p-6">main</main>
+        <main className="flex-1  p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                width={104}
+                height={104}
+                src="/justice-cover.jpg"
+                alt="Capa do álbum de Justice"
+              />
+              <strong>Justice</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+        </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
     </div>
-  )
+  );
 }
