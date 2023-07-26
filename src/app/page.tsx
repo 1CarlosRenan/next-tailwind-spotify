7c615abfab32f19471bc084cc44d6448e16084d3
Carlos Renan <carlosrenan96@gmail.com>
2023-07-25 21:21:30 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import {
   ChevronLeft,
   ChevronRight,
   Play,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Repeat,
+  Mic2,
+  LayoutList,
+  Laptop2,
+  Volume,
+  Maximize2,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -174,11 +183,13 @@ export default function Home() {
             </a>
           </div>
 
-          <h2 className="font-semibold text-2xl mt-10">Made For Carlos Renan</h2>
+          <h2 className="font-semibold text-2xl mt-10">
+            Made For Carlos Renan
+          </h2>
 
           <div className="grid grid-cols-5 gap-4 mt-4">
             <a className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-1  ">
-            <Image
+              <Image
                 className="w-full"
                 width={120}
                 height={120}
@@ -186,10 +197,12 @@ export default function Home() {
                 alt="Capa do álbum de Justice"
               />
               <strong className="font-semibold">Justice Disc One</strong>
-              <span className="text-xs text-zinc-500 ">Wallos, COIN, girl in red and more</span>
+              <span className="text-xs text-zinc-500 ">
+                Wallos, COIN, girl in red and more
+              </span>
             </a>
             <a className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-1  ">
-            <Image
+              <Image
                 className="w-full"
                 width={120}
                 height={120}
@@ -197,10 +210,12 @@ export default function Home() {
                 alt="Capa do álbum de Justice"
               />
               <strong className="font-semibold">Justice Disc One</strong>
-              <span className="text-xs text-zinc-500 ">Wallos, COIN, girl in red and more</span>
+              <span className="text-xs text-zinc-500 ">
+                Wallos, COIN, girl in red and more
+              </span>
             </a>
             <a className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-1  ">
-            <Image
+              <Image
                 className="w-full"
                 width={120}
                 height={120}
@@ -208,10 +223,12 @@ export default function Home() {
                 alt="Capa do álbum de Justice"
               />
               <strong className="font-semibold">Justice Disc One</strong>
-              <span className="text-xs text-zinc-500 ">Wallos, COIN, girl in red and more</span>
+              <span className="text-xs text-zinc-500 ">
+                Wallos, COIN, girl in red and more
+              </span>
             </a>
             <a className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-1  ">
-            <Image
+              <Image
                 className="w-full"
                 width={120}
                 height={120}
@@ -219,10 +236,12 @@ export default function Home() {
                 alt="Capa do álbum de Justice"
               />
               <strong className="font-semibold">Justice Disc One</strong>
-              <span className="text-xs text-zinc-500 ">Wallos, COIN, girl in red and more</span>
+              <span className="text-xs text-zinc-500 ">
+                Wallos, COIN, girl in red and more
+              </span>
             </a>
             <a className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-1  ">
-            <Image
+              <Image
                 className="w-full"
                 width={120}
                 height={120}
@@ -230,14 +249,56 @@ export default function Home() {
                 alt="Capa do álbum de Justice"
               />
               <strong className="font-semibold">Justice Disc One</strong>
-              <span className="text-xs text-zinc-500 ">Wallos, COIN, girl in red and more</span>
+              <span className="text-xs text-zinc-500 ">
+                Wallos, COIN, girl in red and more
+              </span>
             </a>
           </div>
-
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image
+            width={56}
+            height={56}
+            src="/justice-cover.jpg"
+            alt="Capa do álbum de Justice"
+          />
+          <div className="flex flex-col">
+            <strong className="font-normal">DVNO</strong>
+            <span className="text-xs text-zinc-400">Justice</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-3">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:00</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="h-full w-1/2 bg-green-400 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={20}/>
+          <LayoutList size={20}/>
+          <Laptop2 size={20}/>
+          <div className="flex items-center gap-2">
+            <Volume size={20}/>
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="h-full w-1/2 bg-green-400 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20}/>
+        </div>
       </footer>
     </div>
   );
